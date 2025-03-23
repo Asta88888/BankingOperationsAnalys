6636b62e82c2556ea3dd3fb48a333fb8074e478e
Anastasiya Soloviova <mafanya.as@mail.ru>
2025-03-23 20:43:07 +0300

    fixed without main

---
+++ main.py
@@ -0,0 +1,21 @@
+from src.views import get_main_page_info
+from src.reports import spending_by_category
+from src.services import profitable_cashback
+from src.utils import path_excel
+import pandas as pd
+
+
+def main():
+    transactions_df = pd.read_excel(path_excel)
+    date = "2021-09-11 13:27:52"
+    result_json = get_main_page_info(transactions_df, date)
+    print(result_json)
+    category = "Супермаркеты"
+    report = spending_by_category(transactions_df, category, date="2021-12-31")
+    print(report)
+    cashback = profitable_cashback(transactions_df, 2021, 11)
+    print(cashback)
+
+
+if __name__ == "__main__":
+    main()
